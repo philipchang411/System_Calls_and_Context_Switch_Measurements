cefd734adff0f85d88b3d5a39e692c908d7b6295
philipchang411 <philipchang@mail.usf.edu>
2020-01-27 11:21:05 -0500

    Minor Update
    Changed main.c to system_call.c
    added in some comments

--- system_call.c
+++ system_call.c
@@ -5,33 +5,40 @@ double arrayIteration();
 
 int main()
 {
-    clock_t begin = clock();
+    //Begins the clock to get the total run time of the program
+    //clock_t begin = clock();
     double arrayTime;
     arrayTime= arrayIteration();
-    clock_t end = clock();
-    double total_time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    //clock_t end = clock();
+    //double total_time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
     printf("Total time: %f ms \n", arrayTime);
 }
 
 double arrayIteration()
 {
+    //Opens the file that the results are printed to in order to see the increase in time that a larger structure takes to navigate
     FILE *file;
     file = fopen("Record.txt", "w");
-    fprintf(file, "In Array Iteration: \n");
+    fprintf(file, "Array Iteration: \n");
     int storage[1000000];
 
+    //Starts the clock when the reading and writing begins 
     clock_t begin = clock();
     int size;
     size = sizeof(storage)/sizeof(storage[0]);
+
+    //for loop writes to the array and fills it in 
     for(int i = 0; i < size; i++)
     {
-        //printf("WRITING NOW\n");
+        //writing
         storage[i] = i;
         if(i%10000 == 0)
         {
+            //only counds the time when it starts reading
             clock_t readingTimeStart = clock();
             for(int j = 0; j < i; j++)
             {
+                //reading portion
                 int temp;
                 temp = storage[j]; 
             }
